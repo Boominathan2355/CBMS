32c99e31a3f8d71955cc40468f7ed5d41156f646
boominathan2355 <boominathanalagirisamy@gmail.com>
2025-12-14 13:17:41 +0530

    updated  webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,62 +1,65 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.[contenthash].js',
-        clean: true,
-        publicPath: '/'
+  entry: "./src/index.jsx",
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.[contenthash].js",
+    publicPath: "/CBMS/",
+    clean: true
+  },
+
+  mode: process.env.NODE_ENV || "development",
+
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: "asset/resource"
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"   // ✅ FIX
+    }),
+    new Dotenv({
+      systemvars: true
+    })
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public")
     },
-    mode: process.env.NODE_ENV || 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|ico)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
-        new Dotenv({
-            systemvars: true, // load system environment variables as well
-        }),
-    ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        port: 3000,
-        server: 'https',
-        open: true,
-        hot: true,
-        historyApiFallback: true,
-        proxy: [
-            {
-                context: ['/api'],
-                target: 'http://localhost:5000',
-                changeOrigin: true,
-                secure: false,
-            },
-        ],
-    },
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    proxy: [
+      {
+        context: ["/api"],
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false
+      }
+    ]
+  }
 };
